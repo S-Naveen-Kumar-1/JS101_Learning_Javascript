4f73346ae5d8b39aa43e46fcda006bdeb0baa8b5
S NAVEEN KUMAR <navee1110@gmail.com>
2023-04-15 09:49:19 +0530

    conditional-operator

---
+++ assignment-2/problem1.js
@@ -0,0 +1,7 @@
+var number = 8;
+if(number%3==0){
+  console.log("divisible by 3");
+}
+else{
+  console.log("not divisible by 3");
+}
---
+++ assignment-2/problem3.js
@@ -0,0 +1,11 @@
+var a = 5;
+var b = 5;
+if(a>b){
+  console.log("a is greater");
+}
+else if(b>a){
+  console.log("b is greater");
+}
+else{
+  console.log("both are same");
+}
