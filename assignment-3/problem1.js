8a81a2196af2a36880d905e8adb9c1e3d30ea04d
S NAVEEN KUMAR <navee1110@gmail.com>
2023-04-15 11:26:12 +0530

    logical-ternary

---
+++ assignment-3/problem1.js
@@ -0,0 +1,9 @@
+var yob = 1997;
+var age = 2023-yob;
+
+if(age>=13 && age<=19){
+  console.log("Teenage");
+}
+else if(age>=20 && age<=29){
+  console.log("Twenties");
+}
---
+++ assignment-3/problem5.js
@@ -0,0 +1,21 @@
+var day = "sun";
+
+switch(day){
+  case "mon" : console.log("Monday");
+    break;
+    case "tue" : console.log("Tuesday");
+    break;
+    case "wed" : console.log("Wednesday");
+    break;
+    case "thu" : console.log("Thursday");
+    break;
+    case "fri" : console.log("Friday");
+    break;
+    case "sat" : console.log("Saturday");
+    break;
+    case "sun" : console.log("Sunday");
+    break;
+    default : console.log("invalid")
+    break;
+    
+}
